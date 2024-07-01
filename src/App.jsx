def1d0af4691f51863cc807a6055ceff041198c3
meShailu <123243572+meShailu@users.noreply.github.com>
2024-07-01 07:28:06 +0200

    logo impldemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
 import "./App.css";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Header from "./components/Header";
 
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+      </div>
+
+      <ButtonGradient />
+    </>
+  );
 }
