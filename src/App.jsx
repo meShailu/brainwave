fa902f7ed558e87de8e5f91bd8a74c9d8ae07685
meShailu <123243572+meShailu@users.noreply.github.com>
2024-07-02 21:51:41 +0200

    Section component created

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import ButtonGradient from "./assets/svg/ButtonGradient";
 
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 export default function App() {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
+        <Hero />
       </div>
       <ButtonGradient />
     </>
