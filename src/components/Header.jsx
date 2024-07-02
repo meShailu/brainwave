e6b281dae4918a3dfb4b7c47e37cef40266e333e
meShailu <123243572+meShailu@users.noreply.github.com>
2024-07-02 09:02:19 +0200

    some minor fixes with nav bar done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,12 +7,12 @@ import Button from "./Button";
 const Header = () => {
   const pathname = useLocation();
   return (
-    <div className="fixed top-0 left-0 w-full z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
+    <div className="fixed top-0 left-0 w-full z-50  border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm bg-n-8/90 backdrop-blur-sm">
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a className="block w-[12rem] xl:mr-8" href="#hero">
           <img src={brainwave} width={190} height={40} alt="Brainwave" />
         </a>
-        <nav className="hidden fixed top-0 right-o left-0 bottom-0 bg-n-8 lg:static lg:flex lg:max-auto lg:bg-transparent">
+        <nav className="hidden fixed top-0 right-0 left-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent">
           <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
             {navigation.map((item) => (
               <a
