2d109e908d751a5af24fc3779d75d6eb6b029ba7
meShailu <123243572+meShailu@users.noreply.github.com>
2024-07-02 22:06:04 +0200

    heading done with curve image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,36 @@
 import React from "react";
 import Section from "./Section";
+import { curve } from "../assets";
 
 const Hero = () => {
-  return <Section className="pt-[12rem] -m ">Hero</Section>;
+  return (
+    <Section
+      className="pt-[12rem] -mt-[5.25]"
+      crosses
+      crossesOffset="lg:translate-y-[5.25rem]"
+      customPaddings
+      id="hero"
+    >
+      <div className="container relative">
+        <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb:[6rem]">
+          <h1 className="h1 mb-6">
+            {" "}
+            Explore the Possibilities of AI Chatting with BrainwaveCurve
+            <span className="inline-block relative">
+              Brainwave
+              <img
+                src={curve}
+                className="absolute top-full left-0 w-full xl:-mt-2"
+                width={624}
+                height={28}
+                alt="Curve"
+              />
+            </span>
+          </h1>
+        </div>
+      </div>
+    </Section>
+  );
 };
 
 export default Hero;
