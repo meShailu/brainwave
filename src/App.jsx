2a57b7c03642037600565d341747870d66f08217
meShailu <123243572+meShailu@users.noreply.github.com>
2024-07-04 07:29:46 +0200

    benefit component created and heading is rendered

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefits from "./components/Benefits";
 
 import Header from "./components/Header";
 import Hero from "./components/Hero";
@@ -10,6 +11,7 @@ export default function App() {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
+        <Benefits />
       </div>
       <ButtonGradient />
     </>
---
+++ src/components/Benefits.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Section from "./Section";
+import Heading from "./Heading";
+
+const Benefits = () => {
+  return (
+    <Section id="features">
+      <div className="contaner relative z-2">
+        <Heading
+          className="md:max-w-md lg:max-w-2xl"
+          title="Chat Smarter,Not Harder with Brainwave"
+        />
+      </div>
+    </Section>
+  );
+};
+
+export default Benefits;
