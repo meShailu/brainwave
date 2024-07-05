e17725e497bf9d2b244955e4a81a02068121b22c
meShailu <123243572+meShailu@users.noreply.github.com>
2024-07-05 10:21:14 +0200

    minor addition done in smartestAI

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,6 +3,7 @@ import Section from "./Section";
 import Heading from "./Heading";
 import { check, service1 } from "../assets";
 import { brainwaveServices } from "../constants";
+import Generating from "./Generating";
 
 const Services = () => {
   return (
@@ -41,6 +42,7 @@ const Services = () => {
                 ))}
               </ul>
             </div>
+            <Generating className="absolute left-4 right-4 bottom-4 border-n-1/10 border lg:left-1/2 lg-right-auto lg:bottom-8 lg:-translate-x-1/2" />
           </div>
         </div>
       </div>
