cdcf69b9368e0d595d8add857b6d1d1aee66437c
meShailu <123243572+meShailu@users.noreply.github.com>
2024-07-05 11:46:57 +0200

    sphere created in pricing component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Collaboration from "./components/Collaboration";
 
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Pricing from "./components/Pricing";
 import Services from "./components/Services";
 
 export default function App() {
@@ -16,6 +17,7 @@ export default function App() {
         <Benefits />
         <Collaboration />
         <Services />
+        <Pricing />
       </div>
       <ButtonGradient />
     </>
