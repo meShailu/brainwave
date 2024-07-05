a8bdb469e8af86eb2bdcece1ec70f70c091f3ec0
meShailu <123243572+meShailu@users.noreply.github.com>
2024-07-05 13:36:03 +0200

    footer done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
+import Footer from "./components/Footer";
 
 import Header from "./components/Header";
 import Hero from "./components/Hero";
@@ -20,6 +21,7 @@ export default function App() {
         <Services />
         <Pricing />
         <Roadmap />
+        <Footer />
       </div>
       <ButtonGradient />
     </>
