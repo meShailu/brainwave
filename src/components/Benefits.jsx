4a0e2cfc28d2d448961dd56cde6953e7255712ce
meShailu <123243572+meShailu@users.noreply.github.com>
2024-07-05 06:56:15 +0200

    benefit section done

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -4,6 +4,7 @@ import Heading from "./Heading";
 import { benefits } from "../constants";
 import Arrow from "../assets/svg/Arrow";
 import { GradientLight } from "./design/Benefits";
+import ClipPath from "../assets/svg/ClipPath";
 
 const Benefits = () => {
   return (
@@ -23,7 +24,7 @@ const Benefits = () => {
               key={item.id}
             >
               <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
-                <h5 className="h2 mb-5">{item.title}</h5>
+                <h5 className="h5 mb-5">{item.title}</h5>
                 <p className="body-2 mb-6 text-n-3">{item.text}</p>
                 <div className="flex items-center mt auto">
                   <img
@@ -39,7 +40,23 @@ const Benefits = () => {
                 </div>
               </div>
               {item.light && <GradientLight />}
-              <div></div>
+              <div
+                className="absolute inset-0.5 bg-n-8"
+                style={{ clipPath: "url(#benefits)" }}
+              >
+                <div className="absolute inset-0 opacity-0 transition-opacity hover:opacity-10">
+                  {item.imageUrl && (
+                    <img
+                      src={item.imageUrl}
+                      width={380}
+                      height={362}
+                      alt={item.title}
+                      className="w-full h-full object-cover"
+                    />
+                  )}
+                </div>
+              </div>
+              <ClipPath />
             </div>
           ))}
         </div>
