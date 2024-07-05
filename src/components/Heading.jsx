b13e523bda98a36cba2abc2508d0aa79d29524ea
meShailu <123243572+meShailu@users.noreply.github.com>
2024-07-05 06:42:05 +0200

    card is done with gradient light

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Heading = ({ className, title }) => {
   return (
-    <div className={`${className} max-w-[50rem] mx-auto mv-12 lg:mb-20`}>
+    <div className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20`}>
       {title && <h2 className="h2">{title}</h2>}
     </div>
   );
